acb31bb5d507536cd4a1e9f3526907a95c86cc08
AlbertKhoo <khookh324416@student.usm.my>
2025-11-05 15:49:23 +0800

    Update the firebaseConfig to apply environment variables

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBrYGy-PkbAvhgmrYYmQ0vlmf43hjgMRTw",
-  authDomain: "hotelfix-10e2c.firebaseapp.com",
-  projectId: "hotelfix-10e2c",
-  storageBucket: "hotelfix-10e2c.firebasestorage.app",
-  messagingSenderId: "720081265245",
-  appId: "1:720081265245:web:675e5b77e56cb47b8da3b4",
-  measurementId: "G-PHDYKJBPJS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
