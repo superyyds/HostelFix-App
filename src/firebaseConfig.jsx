dd04357e4f978b5dc715073853d680cc2628d1c0
AlbertKhoo <khookh324416@student.usm.my>
2025-11-05 15:52:01 +0800

    Update firebaseConfig to correct version

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,3 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
